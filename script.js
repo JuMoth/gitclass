b0a3899d961359c7563ea3cdd362a11dfd6a6d01
JuMoth <tkdlqjqkd@gmail.com>
2023-08-15 14:02:59 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('hello javascript');
+
+// 변수 선언과 함수 작성
+
+var age = 25;
+let year = 2023; // 숫자
+const point = 250;
+const today = '광복절'; // 문자
+
+const pass = true;
+
+if(pass) {
+    console.log('hello');
+}
+
+// data type.
+
+// Number = 1, 2, 3, 4, 5, 7.5, 8.5
+// String = 'hello world'
+// Boolean = true/false /참, 거짓
+
+// Null, Undefined, Object
+
